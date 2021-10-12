9519e9ca5e1641909dac498ec37a86d00d278d0c
Yash Poojary <yashspoojary245@gmail.com>
2021-10-12 19:48:55 +0530

    Initial Commit

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,15 +7,86 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
 struct ContentView: View {
+    @State private var results = [Result]()
+
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+
+    func loadData() {
+
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedRespone = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        results = decodedRespone.results
+                    }
+
+                    return
+                }
+            }
+
+            print("Fetch Failed: \(error?.localizedDescription ?? "Unkown Error")")
+        }.resume()
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+//class User: ObservableObject, Codable {
+//
+//    @Published var name = "Paul Hudson"
+//
+//
+//    enum CodingKeys: CodingKey {
+//        case name
+//    }
+//
+//    required init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        name = try container.decode(String.self, forKey: .name)
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(name, forKey: .name)
+//    }
+//
+//
+//}
